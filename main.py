e827050473b9283c53fb418ebc394358c82f6056
FrederikReigstad <Frederik9876@gmail.com>
2023-03-13 12:15:30 +0100

    data can be recived form mqqt

--- main.py
+++ main.py
@@ -4,38 +4,51 @@ from sqlalchemy.future import engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# This happens when connecting
+def on_connect(mqttc, obj, flags, rc):
+ print("rc: " + str(rc))
+
+# On subscribing to messages
+def on_subscribe(mqttc, obj, mid, granted_qos):
+ print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
 # Connect to MQTT
 def on_message(client, userdata, msg):
+    print(str(msg.topic) + " " + str(msg.qos) + " " + str(msg.payload))
     # Parse message and extract data
-    data = msg.payload.decode()
+    #data = msg.payload.decode()
     # Connect to the database
-    engine = create_engine('DATABASE_URL')
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    #engine = create_engine('DATABASE_URL')
+    #Session = sessionmaker(bind=engine)
+    #session = Session()
     # Insert data into the database
-    record = Data(value=data)
-    session.add(record)
-    session.commit()
-    session.close()
+    #record = Data(value=data)
+    #session.add(record)
+    #session.commit()
+    #session.close()
 
-
+myhost="mqtt.flespi.io"
 client = mqtt.Client()
 client.on_message = on_message
-client.connect("mqtt.flespi.io", 1883)
+client.on_subscribe = on_subscribe
+client.on_connect = on_connect
+
 client.username_pw_set("T0jLbGxLz6LQVQPXDKFJNPIs17LM1DUKt3lvzG4ZBFDmmi9NQDkriSJ9PlJGOsh5","")
-client.subscribe("BlindData/warning")
+client.connect(myhost, 1883)
+client.subscribe("BlindData/#", 1)
+client.loop_forever()
+
 
 # Define the database model
-Base = declarative_base()
+#Base = declarative_base()
 
 
-class Data(Base):
-    __tablename__ = 'data'
-    id = Column(Integer, primary_key=True)
-    value = Column(String)
+#class Data(Base):
+    #__tablename__ = 'data'
+    #id = Column(Integer, primary_key=True)
+    #value = Column(String)
 
 
-Base.metadata.create_all(engine)
+#Base.metadata.create_all(engine)
 
-client.loop_forever()
+
