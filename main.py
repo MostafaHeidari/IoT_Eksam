f906f4846744cdfe93eb52a2b6fc7b889aec4be6
FrederikReigstad <Frederik9876@gmail.com>
2023-03-13 13:48:46 +0100

    big updarte date i saced to database

--- main.py
+++ main.py
@@ -7,19 +7,31 @@ from sqlalchemy.future import engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+DATABASE_FILE = 'BlindDb.db'
+
 # This happens when connecting
 def on_connect(mqttc, obj, flags, rc):
- print("rc: " + str(rc))
+    print("rc: " + str(rc))
 
 # On subscribing to messages
 def on_subscribe(mqttc, obj, mid, granted_qos):
- print("Subscribed: " + str(mid) + " " + str(granted_qos))
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
 # Connect to MQTT
-def on_message(client, userdata, msg):
+def on_message(client, user_data, msg):
     # Parse message and extract data
-    data = msg.payload.decode()
+    data = msg.payload.decode('utf-8')
     print(data)
+    print(msg.topic)
+
+    db_conn = user_data['db_conn']
+    if data != "{Low}":
+        sql = 'INSERT INTO blind_data (topic, dangerlevel, location, created_at) VALUES (?, ?, ?, ?)'
+        cursor = db_conn.cursor()
+        cursor.execute(sql, (msg.topic, data, data, int(time())))
+        db_conn.commit()
+        cursor.close()
+
 
     # Connect to the database
     #engine = create_engine('DATABASE_URL')
@@ -31,16 +43,36 @@ def on_message(client, userdata, msg):
     #session.commit()
     #session.close()
 
-myhost="mqtt.flespi.io"
-client = mqtt.Client()
-client.on_message = on_message
-client.on_subscribe = on_subscribe
-client.on_connect = on_connect
+def main():
+    db_conn = sqlite3.connect(DATABASE_FILE)
+    sql = """
+    CREATE TABLE IF NOT EXISTS blind_data (    
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        topic TEXT NOT NULL,
+        dangerlevel TEXT NOT NULL,
+        location TEXT NOT NULL,
+        created_at INTEGER NOT NULL
+    )
+    """
+    cursor = db_conn.cursor()
+    cursor.execute(sql)
+    cursor.close()
 
-client.username_pw_set("T0jLbGxLz6LQVQPXDKFJNPIs17LM1DUKt3lvzG4ZBFDmmi9NQDkriSJ9PlJGOsh5","")
-client.connect(myhost, 1883)
-client.subscribe("BlindData/#", 1)
+    myhost="mqtt.flespi.io"
+    client = mqtt.Client()
 
+
+    client.on_message = on_message
+    client.on_subscribe = on_subscribe
+    client.on_connect = on_connect
+
+    client.username_pw_set("T0jLbGxLz6LQVQPXDKFJNPIs17LM1DUKt3lvzG4ZBFDmmi9NQDkriSJ9PlJGOsh5","")
+    client.connect(myhost, 1883)
+    client.user_data_set({'db_conn': db_conn})
+
+    client.subscribe("BlindData/#", 1)
+
+    client.loop_forever()
 
 
 # Define the database model
@@ -55,5 +87,4 @@ client.subscribe("BlindData/#", 1)
 
 #Base.metadata.create_all(engine)
 
-
-client.loop_forever()
+main()
